91d3e194270b317e83fc9bf4494da8d1c9e0045c
hamza <hamza427133@gmail.com>
2024-02-29 00:09:21 +0500

    assignment 18

---
+++ app.js
@@ -0,0 +1,119 @@
+// chapter 9
+
+// Q1
+
+// var city =prompt("Enter The Name of City");
+
+// if ("karachi"){
+//     document.write(true);
+// }
+
+// else {
+//     document.write(false);
+// }
+
+
+// Q2
+
+// var gender =prompt("Enter Your Gender")
+
+// if("male"){
+//     document.write("Good Morning Sir")
+
+// }
+
+// else {
+//     document.write("NOt SURE")
+// }
+
+// var gender =prompt("Enter Your Gender")
+
+// if("female"){
+//     document.write("Good Morning Mam")
+
+// }
+
+// else {
+//     document.write("NOt SURE")
+// }
+
+
+
+
+// Q3
+
+// chapter 9
+
+// Q3
+
+// var color = prompt("Enter The Color of Road Traffic Signal");
+
+// if (color.toLowerCase() === "red") {
+//     document.write("Stop");
+// } else if (color.toLowerCase() === "yellow") {
+//     document.write("Prepare to Stop");
+// } else if (color.toLowerCase() === "green") {
+//     document.write("Go");
+// } else {
+//     document.write("Invalid Color");
+// }
+
+
+// Q4
+
+// var fuel = parseFloat(prompt("Enter The Remaining Fuel in Your Car (in litres)"));
+
+// if (fuel < 0.25) {
+//     document.write("Please refill the fuel in your car");
+// } else {
+//     document.write("Your car has enough fuel");
+// }
+
+
+// Q5
+
+// a. vara = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// b. var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+
+// c. var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+
+// d. var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+
+// e. if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+
+// f. if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
